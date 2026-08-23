0eb4c129e791db8ef8d1145001e4ae93ebf7c0d1
Suman Saha <8759127658itisaha@gmail.com>
2026-08-23 20:22:36 +0530

    New React Template

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import carouselReducer from './features/carouselSlice';
-
-export const store = configureStore({
-  reducer: {
-    carousel: carouselReducer,
-    // ... other reducers ...
-  },
-});
